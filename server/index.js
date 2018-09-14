7dd575eccf4f1a0d8f6cf48c0ae649dbf1134ec1
Daniel Corner <dcorn068@gmail.com>
2018-09-14 12:17:19 -0400

    add create, read, update queries and resolvers

--- server/index.js
+++ server/index.js
@@ -1,40 +1,71 @@
 const { GraphQLServer } = require('graphql-yoga');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/test2');
+mongoose.connect('mongodb://localhost/test3');
 
 const Art = mongoose.model('Art', {
   title: String,
   caption: String,
+  price: Number,
   avail: Boolean
 });
 
 const typeDefs = `
-  type Query {
-    hello(name: String): String!
-  }
   type Art {
     id: ID!
     title: String!
     caption: String
+    price: Float
     avail: Boolean!
   }
+  type Query {
+    allArt: [Art]
+    art(id: ID!): Art
+  }
   type Mutation {
-    createArt(title: String!, caption: String): Art
+    createArt(
+      title: String!, 
+      caption: String, 
+      price: Float
+      ): Art
+    updateArt(
+      id: ID!, 
+      title: String, 
+      caption: String, 
+      price: Float, 
+      avail: Boolean
+      ): Boolean
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: (_, { name }) => `Hello ${name || 'World'}`
+    allArt: () => Art.find(),
+    art: id => Art.findOne({ id: id })
   },
   Mutation: {
     // createArt: async (_, {title}) => {
-    createArt: async (_, { title, caption }) => {
-      caption = caption || ''; // if no caption, set to blank
-      const art = new Art({ title, caption, avail: true });
+    createArt: async (_, { title, caption, price }) => {
+      caption = caption || ''; // if no value, set to falsy
+      price = price || 0;
+      const art = new Art({ title, caption, price, avail: true });
       await art.save();
       return art;
+    },
+    updateArt: async (
+      _,
+      // https://coursework.vschool.io/mongoose-crud/
+      // the change to be made. Mongoose will smartly combine your existing
+      // document with this change, which allows for partial updates too
+      { id, title, caption, price, avail }
+    ) => {
+      if (avail == undefined) {
+        // if null or undefined don't return the availability
+        await Art.findOneAndUpdate(id, { title, caption, price });
+      } else {
+        await Art.findOneAndUpdate(id, { title, caption, price, avail });
+      }
+      return true;
     }
   }
 };
