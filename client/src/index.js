a2ebb4fb43730dc805954a970fc1e0be27be3e5c
Daniel Corner <dcorn068@gmail.com>
2018-09-16 16:26:46 -0400

    new ApolloClient uri using GCloud server

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,8 @@ import registerServiceWorker from './registerServiceWorker';
 import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-boost';
 const client = new ApolloClient({
-  uri: 'mongodb://admin:password123@ds139919.mlab.com:39919/hl_storefront_db'
+  uri: 'https://hl-storefront-server.appspot.com/'
+  // uri: 'http://localhost:4000'
 });
 
 ReactDOM.render(
