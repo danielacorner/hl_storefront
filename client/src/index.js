a08f424084abbb632811e53f1d99716be2103d95
Daniel Corner <dcorn068@gmail.com>
2018-09-16 16:12:29 -0400

    New Apollo client uri

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import registerServiceWorker from './registerServiceWorker';
 import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-boost';
 const client = new ApolloClient({
-  uri: 'http://localhost:4000'
+  uri: 'mongodb://admin:password123@ds139919.mlab.com:39919/hl_storefront_db'
 });
 
 ReactDOM.render(
